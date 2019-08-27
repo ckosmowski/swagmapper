32a3b35f281dd4ab988c98631d4d87765c8da996
Christian Kosmowski <ksmwsk@gmail.com>
2019-08-27 20:51:20 +0200

    build sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -15,8 +15,8 @@ fi
 if [ "$TRAVIS_PULL_REQUEST" != "false" ];
 then
 	echo "Skip integration tests in pull request builds"
-	./gradlew clean build -x integrationTest
+	./gradlew clean build
 else
 	echo "Temporarily skip integration tests in all builds. Too heavy for Travis"
-	./gradlew clean build -x integrationTest
+	./gradlew clean build
 fi
