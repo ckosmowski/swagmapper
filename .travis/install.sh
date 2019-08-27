d38f96d1b239c849360e70ca5f36cf993d3b96f6
Christian Kosmowski <ksmwsk@gmail.com>
2019-08-27 21:07:21 +0200

    Use env variables for key locations

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,5 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-    openssl aes-256-cbc -K $encrypted_e66af61e534c_key -iv $encrypted_e66af61e534c_iv -in B27871CF38A3D5179C445ABC95002DCA39516032.gpg.enc -out B27871CF38A3D5179C445ABC95002DCA39516032.gpg -d
+    openssl aes-256-cbc -K $encrypted_e66af61e534c_key -iv $encrypted_e66af61e534c_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
 fi
