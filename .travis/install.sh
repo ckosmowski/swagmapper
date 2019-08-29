cdd75947e3cb4e802e4ed9a74dace53c980d4778
Christian Kosmowski <ksmwsk@gmail.com>
2019-08-29 18:57:16 +0200

    Next try

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,5 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-    echo $enc_key | gpg --passphrase-fd 0 secret.gpg.gpg
+    echo $enc_key | gpg --passphrase-fd 0 $ENCRYPTED_GPG_KEY_LOCATION
 fi
